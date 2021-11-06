bf877370a58c758d592d4af0030e86a6b470d039
nevstafev <evstafev.n.e@gmail.com>
2021-11-06 19:37:06 +0300

    revert

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -2,24 +2,6 @@ import { HttpService } from '@nestjs/axios';
 import { All, Controller, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { Method } from 'axios';
-import { ParamsDictionary } from 'express-serve-static-core';
-
-const getUrlWithoutPrefix = (url: string, urlPrefix: string | null): string => {
-  if (!urlPrefix || urlPrefix.length === 0) {
-    return url;
-  }
-  return url.split(`${urlPrefix}/`)[1];
-};
-
-const getRecipientFromParams = (params: ParamsDictionary): string | null => {
-  const paramsString = params[0];
-  if (!paramsString || paramsString.length === 0) {
-    return null;
-  }
-  return getUrlWithoutPrefix(paramsString, process.env.URL_PREFIX).split(
-    '/',
-  )[0];
-};
 
 @Controller()
 export class ApiController {
@@ -29,8 +11,7 @@ export class ApiController {
     @Req() req: Omit<Request, 'method'> & { method: Method },
     @Res() res: Response,
   ) {
-    const recipientParam = getRecipientFromParams(req.params);
-
+    const recipientParam = req.params[0].split('/')[0];
     const recipientUrl = process.env[recipientParam];
 
     if (recipientUrl) {

00eff1af7021acee3f09f5cabad48e8090e1302e
nevstafev <evstafev.n.e@gmail.com>
2021-11-06 22:59:29 +0300

    add caching

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,23 +1,50 @@
 import { HttpService } from '@nestjs/axios';
-import { All, Controller, Req, Res } from '@nestjs/common';
+import {
+  All,
+  CACHE_MANAGER,
+  Controller,
+  Inject,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import { Method } from 'axios';
+import { Cache } from 'cache-manager';
+
+const isCacheable = (req: Request): boolean => {
+  const urlPats = req.originalUrl.split('/');
+  if (urlPats.length !== 2) {
+    return false;
+  }
+  const recipient = urlPats[1];
+  return Boolean(process.env[`${recipient}_cache`]);
+};
 
 @Controller()
 export class ApiController {
-  constructor(private httpService: HttpService) {}
+  constructor(
+    private httpService: HttpService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
   @All('*')
   async callApi(
     @Req() req: Omit<Request, 'method'> & { method: Method },
     @Res() res: Response,
   ) {
-    const recipientParam = req.originalUrl.split('/')[1];
-    const recipientUrl = process.env[recipientParam];
+    const recipient = req.originalUrl.split('/')[1];
+    const recipientUrl = process.env[recipient];
 
     if (recipientUrl) {
       try {
+        const cacheable = isCacheable(req);
+        if (cacheable) {
+          const value = await this.cacheManager.get(recipient);
+
+          if (value) {
+            return res.json(value);
+          }
+        }
         const url = `${recipientUrl}${req.originalUrl}`;
-        console.log(`Calling url ${url}`);
 
         const apiResponse = await this.httpService
           .request({
@@ -27,7 +54,9 @@ export class ApiController {
             ...(Object.keys(req.body).length > 0 ? { data: req.body } : {}),
           })
           .toPromise();
-
+        if (cacheable) {
+          this.cacheManager.set(recipient, apiResponse.data, { ttl: 120 });
+        }
         res.json(apiResponse.data);
       } catch (e) {
         res.status(502).end('Cannot process request');
--- bff-service/src/app.module.ts
+++ bff-service/src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { ApiController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HttpModule],
+  imports: [ConfigModule.forRoot(), HttpModule, CacheModule.register()],
   controllers: [ApiController],
 })
 export class AppModule {}
